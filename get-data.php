193e810b35aad562960e6224816d913a3137166d
dstale77 <Dstale77@yahoo.com>
2018-04-12 00:18:55 -0400

    another version

--- get-data.php
+++ get-data.php
@@ -4,36 +4,125 @@ $username = "root";
 $password = "";
 $dbname = "fcc_data";
 
-// Create connection
-//$conn = new mysqli($servername, $username, $password, $dbname);
+/*
+//which type of function to call
+$fun=$_POST["Func"];
+
+//range of speeds
+$Mim=$_POST["Mim"];
+$Mam= $_POST["Mam"];
+
+//zip search
+$gZip1=$_POST["gZip1"];
+$gZip2=$_POST["gZip2"];
+
+//uppload or download
+$type=$_POST["type"];
+
+//getLatLong($Mim,$Mam);
+
+//will need: min speed max speed
+*/
+
+getLatLong(0,1);
+//need speed_refernce, fcc_database, block_reffernce... mainly
+
+//Find all data where given Zip
+	//compare zip with fipsID, get all fips data(up,down,company) then get lat & long
+
+//find all data where range is true, type is given,
+	// get fips(id), get coords
+
+$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
+FROM fcc_database
+RIGHT JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid
+where fcc_database.typicdown >8";
 
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+function getLatLong($Mim,$Mam){
+	// Create connection
+	//echo json_encode($Mim);
+	//echo json_encode($Mam);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fcc_data";
 
-$sql = "SELECT latitude,longitude FROM block_reffernce";
-$result = $conn->query($sql);
-$myArray = array();
-if ($result->num_rows > 0) {
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
 
-   // echo "<table><tr><th>ID</th><th>Name</th></tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-    	 $myArray[] = $row;
 
-        //echo "<tr><td>".$row["latitude"]."</td><td>".$row["longitude"]."</td></tr>";
-    }
-    //echo "</table>";
-} else {
-    echo "0 results";
+	//query
+	$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
+FROM fcc_database
+inner JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid";
+//where fcc_database.typicdown >8
+
+	$result = $conn->query($sql);
+	$myArray = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	 $myArray[] = $row;
+	    }
+	} else {
+	    echo "0 results";
+	}
+	 echo json_encode($myArray);
+
+	$conn->close();
+
+	//will need fullfipsid
 }
 
- echo json_encode($myArray);
+function getZip1(){
+	$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fcc_data";
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
 
-$conn->close();
+
+	//query
+	$sql = "SELECT latitude,longitude FROM block_reffernce";
+	$result = $conn->query($sql);
+	$myArray = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	 $myArray[] = $row;
+	    }
+	} else {
+	    echo "0 results";
+	}
+	 echo json_encode($myArray);
+	$conn->close();
+}
+
+function getZip2(){
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
+
+
+	//query
+	$sql = "SELECT latitude,longitude FROM block_reffernce";
+	$result = $conn->query($sql);
+	$myArray = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	 $myArray[] = $row;
+	    }
+	} else {
+	    echo "0 results";
+	}
+	 echo json_encode($myArray);
+	$conn->close();
+}
+
+
 
 ?>
--- index.php
+++ index.php
@@ -2,78 +2,88 @@
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="map"></div>
-    <script>
-      var map;
+    <head>
+        <style>
+            /* Always set the map height explicitly to define the size of the div
+            * element that contains the map. */
+            #map {
+                height: 80%;
+            }
+            /* Optional: Makes the sample page fill the window. */
+            html, body {
+                height: 100%;
+                margin: 0%;
+                padding: 0%;
+            }
+        </style>
+    </head>
 
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: new google.maps.LatLng(40.900,-77.194)
-        });
-
-        //calls data, then places markers
-        var jsondata = callData();
-
-
-       // document.getElementsByTagName('head')[0].appendChild(script);   
-      }
-
-
-
-     
     
-/*
-      window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-          var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-        }
-      }
-
-*/
+<body>
+<div id="map"></div>
 
 
-    </script>
+
+<script>
+    //create map
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7, center: new google.maps.LatLng(40.900,-77.194) //centers map on PA
+        });
+        var param="Mim=5&Mam=7";
+        //SendRequest(param);//sends data request with variables
+        var jsondata = callData(); //calls data, then places markers
 
 
-     <script>
-        //calls data
+    }
+</script>
+
+
+
+
+ <script>
+    //general functions
+
+    //sends request
+    function SendRequest(param){
+
+        var oReq = new XMLHttpRequest(); //New request object;
+         oReq.open("post", "get-data.php", true)
+
+            oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            console.log(param)
+            var myArr = JSON.parse(this.responseText);
+            marker(myArr);
+
+
+        oReq.onreadystatechange = function() { //Call a function when the state changes.
+            if(oReq.readyState == 4 && oReq.status == 200) { // complete and no errors
+                console.log(oReq.responseText); // some processing here, or whatever you want to do with the response
+                }
+         }
+
+        oReq.send(param);  
+    }
+
+
+    //calls data
     function callData(){
         var oReq = new XMLHttpRequest(); //New request object;
+         oReq.open("get", "get-data.php", true)
+
+
         oReq.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
+                //console.log(this.responseText);
                 var myArr = JSON.parse(this.responseText);
                 marker(myArr);
             };
-        }
-
-        oReq.open("get", "get-data.php", true);
+        };
         oReq.send();  
     }
         
-
-       
+ 
     // places markers
     function marker(jsondata) {
         for(var i = 0; i < jsondata.length; i++) {
@@ -82,30 +92,13 @@
             var marker = new google.maps.Marker({position:latLng, map: map})
         }
     }
+</script>
 
 
 
-
-
-
-    function doNothing() {
-
-    }
-
-
-    function reqListener () {
-      console.log( this.responseText);
-    }
-
-
-    </script>
-
-
-
-
-
-    <script async defer
+<script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCG3IJFse1sdzlssxc9uGB52cWWKod70ZI&callback=initMap">
-    </script>
-  </body>
+</script>
+
+</body>
 </html>
