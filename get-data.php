036cd9b543d5df17d006f6b8dccf2e91336bb24b
dstale77 <Dstale77@yahoo.com>
2018-04-15 15:29:13 -0400

    near final product

--- get-data.php
+++ get-data.php
@@ -1,92 +1,111 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "fcc_data";
-
-/*
-//which type of function to call
-$fun=$_POST["Func"];
 
 //range of speeds
-$Mim=$_POST["Mim"];
-$Mam= $_POST["Mam"];
+if (isset($_POST['MimU'])) //checks if exists
+{ 
+	 $MimU=(int)$_POST["MimU"];
+} 
+if (isset($_POST['MamU'])) 
+{ 
+	$MamU= (int)$_POST["MamU"];
+} 
+if (isset($_POST['MimD'])) //checks if exists
+{ 
+	 $MimD=(int)$_POST["MimD"];
+} 
+if (isset($_POST['MamD'])) 
+{ 
+	$MamD= (int)$_POST["MamD"];
+} 
+
 
 //zip search
-$gZip1=$_POST["gZip1"];
-$gZip2=$_POST["gZip2"];
+if (isset($_POST['gzip'])) 
+{ 
+	$gzip=$_POST["gzip"];
+} 
 
-//uppload or download
-$type=$_POST["type"];
+//unquie id
+if (isset($_POST['OBJid'])) 
+{ 
+	$OBJid=$_POST["OBJid"];
+} 
 
-//getLatLong($Mim,$Mam);
-
-//will need: min speed max speed
-*/
-
-getLatLong(0,1);
-//need speed_refernce, fcc_database, block_reffernce... mainly
-
-//Find all data where given Zip
-	//compare zip with fipsID, get all fips data(up,down,company) then get lat & long
-
-//find all data where range is true, type is given,
-	// get fips(id), get coords
-
-$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
-FROM fcc_database
-RIGHT JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid
-where fcc_database.typicdown >8";
-
-
-function getLatLong($Mim,$Mam){
-	// Create connection
-	//echo json_encode($Mim);
-	//echo json_encode($Mam);
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "fcc_data";
-
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
-
-
-	//query
-	$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
-FROM fcc_database
-inner JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid";
-//where fcc_database.typicdown >8
-
-	$result = $conn->query($sql);
-	$myArray = array();
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-	    	 $myArray[] = $row;
-	    }
-	} else {
-	    echo "0 results";
+//determines which function runs
+if (isset($_POST['Func'])) { 
+	$Func=$_POST["Func"];
+	if ($Func==0){
+		getAll();
 	}
-	 echo json_encode($myArray);
-
-	$conn->close();
-
-	//will need fullfipsid
+	elseif ($Func==1) {
+		getDownload($MimD,$MamD);
+	}
+	elseif ($Func==2) {
+		getUpload($MimU,$MamU);
+	}
+	elseif ($Func==3) {
+		getZip($gzip);
+	}
+	elseif ($Func==4) {
+		getSpecific($OBJid);
+	}
+	elseif ($Func==5){
+		getUpDown($MimU,$MamU,$MimD,$MamD);
+	}
+} 
+else{
+	getall();
 }
 
-function getZip1(){
+//get all data points
+function getAll(){
+	// Create connection
 	$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "fcc_data";
-	// Create connection
+	$username = "root";
+	$password = "";
+	$dbname = "fcc_data";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
 
 
 	//query
-	$sql = "SELECT latitude,longitude FROM block_reffernce";
+	$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid
+	FROM fcc_database
+	inner JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid";
+
+	//create json data
+	$result = $conn->query($sql);
+	$myArray = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	 $myArray[] = $row;
+	    }
+	} else {
+	    echo "0 results";//if none exists
+	}
+	 echo json_encode($myArray);//send json data
+
+	$conn->close();
+}
+
+//get specific zip
+function getZip($zip){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "fcc_data";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
+
+
+	//get specific location
+	$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
+	FROM fcc_database
+	RIGHT JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid
+	where block_reffernce.zip5 = $zip";
+
+
 	$result = $conn->query($sql);
 	$myArray = array();
 	if ($result->num_rows > 0) {
@@ -101,14 +120,23 @@ $dbname = "fcc_data";
 	$conn->close();
 }
 
-function getZip2(){
-	// Create connection
+//get all dataponts within range of upload speeds
+function getUpload($Min,$Max){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "fcc_data";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
 
 
-	//query
-	$sql = "SELECT latitude,longitude FROM block_reffernce";
+	//get all upload
+	$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
+	FROM fcc_database
+	RIGHT JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid
+	where fcc_database.uploadspeed >= $Min and fcc_database.uploadspeed <= $Max";
+
+
 	$result = $conn->query($sql);
 	$myArray = array();
 	if ($result->num_rows > 0) {
@@ -123,6 +151,96 @@ function getZip2(){
 	$conn->close();
 }
 
+//get all dataponts within range of download speeds
+function getDownload($Min,$Max){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "fcc_data";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
 
+
+	//get range of locations
+	$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
+	FROM fcc_database
+	RIGHT JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid
+	where fcc_database.downloadspeed >= $Min and fcc_database.downloadspeed <= $Max";
+
+	//creat json string
+	$result = $conn->query($sql);
+	$myArray = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	 $myArray[] = $row;
+	    }
+	} else {
+	    echo "0 results";
+	}
+	 echo json_encode($myArray);
+	$conn->close();
+}
+
+//get info on specific datapoint
+function getSpecific($Id){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "fcc_data";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
+
+
+	//get specific id
+	$sql = "SELECT * 
+	FROM fcc_database
+	where fcc_database.objectid= '$Id'";
+
+	//query
+	$result = $conn->query($sql);
+	$myArray = array();
+
+	//make json string
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	echo json_encode($row);
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();
+}
+
+function getUpDown($MimU,$MamU,$MimD,$MamD){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "fcc_data";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);} 
+
+
+	//get range of locations
+	$sql = "SELECT block_reffernce.latitude, block_reffernce.longitude, fcc_database.objectid 
+	FROM fcc_database
+	RIGHT JOIN block_reffernce ON fcc_database.fullfipsid = block_reffernce.fullfipsid
+	where fcc_database.downloadspeed >= $MimD and fcc_database.downloadspeed <= $MamD and fcc_database.uploadspeed >= $MimU and fcc_database.uploadspeed <= $MamU";
+
+	//creat json string
+	$result = $conn->query($sql);
+	$myArray = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	 $myArray[] = $row;
+	    }
+	} else {
+	    echo "0 results";
+	}
+	 echo json_encode($myArray);
+	$conn->close();
+}
 
 ?>
--- index.php
+++ index.php
@@ -31,12 +31,25 @@
         map = new google.maps.Map(document.getElementById('map'), {
             zoom: 7, center: new google.maps.LatLng(40.900,-77.194) //centers map on PA
         });
-        var param="Mim=5&Mam=7";
-        //SendRequest(param);//sends data request with variables
-        var jsondata = callData(); //calls data, then places markers
+        var MimU="0";
+        var MamU="4";
 
+        var MimD="5";
+        var MamD="11";
+
+        var gzip="17101";
+        var Func="0";
+        
+
+
+        var param="MimU="+MimU+"&MamU="+MamU+"&MimD="+MimD+"&MamD="+MamD+"&gzip="+gzip+"&Func="+Func;
+       console.log(param);
+        //SendRequest(param);//sends data request with variables
+        var jsondata = SendRequest(param); //calls data, then places markers
+       // var jsondara=callData();
 
     }
+
 </script>
 
 
@@ -45,6 +58,39 @@
  <script>
     //general functions
 
+
+function GetInfo(param){
+
+        var oReq = new XMLHttpRequest(); //New request object;
+         oReq.open("post", "get-data.php", false)
+
+            oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            
+
+        oReq.onreadystatechange = function() { //Call a function when the state changes.
+            if(oReq.readyState == 4 && oReq.status == 200) { // complete and no errors
+                //console.log(this.responseText);
+                //console.log(this.responseText);
+                 var myArr = JSON.parse(this.responseText);
+                 alert("Provider: "+myArr.provname+"\n"+
+                    "Company: "+myArr.hoconame+"\n"+
+                    " download: "+spd(myArr.downloadspeed)+"\n"+
+                    " upload: "+spd(myArr.uploadspeed)+"\n"+
+                    "Category: "+Cat(myArr.end_user_cat)+"\n");
+                 //return myArr;
+                 // marker(myArr);
+
+                 // some processing here, or whatever you want to do with the response
+                }
+         }
+
+        oReq.send(param);
+
+    }
+
+
+
+
     //sends request
     function SendRequest(param){
 
@@ -52,18 +98,22 @@
          oReq.open("post", "get-data.php", true)
 
             oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            console.log(param)
-            var myArr = JSON.parse(this.responseText);
-            marker(myArr);
-
+            
+           
+           
 
         oReq.onreadystatechange = function() { //Call a function when the state changes.
             if(oReq.readyState == 4 && oReq.status == 200) { // complete and no errors
-                console.log(oReq.responseText); // some processing here, or whatever you want to do with the response
+                console.log(this.responseText);
+                //console.log(this.responseText);
+                 var myArr = JSON.parse(this.responseText);
+                  marker(myArr);
+
+                 // some processing here, or whatever you want to do with the response
                 }
          }
 
-        oReq.send(param);  
+        oReq.send(param);
     }
 
 
@@ -86,10 +136,82 @@
  
     // places markers
     function marker(jsondata) {
+        //console.log(jsondata);
         for(var i = 0; i < jsondata.length; i++) {
             var coords =jsondata[i]
+
+            var markdata= coords.objectid
+            
+
+
+
             var latLng= new google.maps.LatLng(coords.latitude, coords.longitude)
-            var marker = new google.maps.Marker({position:latLng, map: map})
+            var marker = new google.maps.Marker({position:latLng, map: map, customInfo:markdata, })
+            //var t = new google.maps.event.addListener(marker, 'click', function() {alert(this.customInfo);});
+            var t = new google.maps.event.addListener(marker, 'click', function(){GetInfo("OBJid="+this.customInfo+"&Func=4")})
+
+        }
+    }
+
+    function spd(id){
+        if(id==1){
+            return "0 - 200 kbps";
+        }
+        else if (id==2){
+            return "200 - 768  kbps";
+        }
+        else if (id==3){
+            return "768 kbps - 1.5 mbps";
+        }
+            else if (id==4){
+            return "1.5 - 3 mbps";
+        }
+            else if (id==5){
+            return "3 - 6 mbps";
+        }
+            else if (id==6){
+            return "6 - 10 mbps";
+        }
+            else if (id==7){
+            return "10 - 25 mbps";
+        }
+            else if (id==8){
+            return "25 - 50 mbps";
+        }
+            else if (id==9){
+            return "50 - 100 mbps";
+        }
+            else if (id==10){
+            return "100 mbps - 1 gbps";
+        }
+            else if (id==11){
+            return "1 gbps -2 gbps";
+        }
+        else{
+            return "error";
+        }
+    }
+
+
+    function Cat(id){
+
+        if (id==1){
+            return "Residential";
+        }
+        else if (id==2){
+            return "Goverment";
+        }
+        else if (id==3){
+            return "small buisness";
+        }
+        else if (id==4){
+            return "medium or large enterprise";
+        }
+        else if (id==5){
+            return "other";
+        }
+        else{
+            return "error";
         }
     }
 </script>
